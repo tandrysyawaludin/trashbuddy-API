ebc75aa5152cf9acb864f2fa32848ea7e2a63369
Unknown <syawaludintandry@yahoo.com>
2018-04-23 11:01:47 +0700

    Update db

--- src/main.rs
+++ src/main.rs
@@ -11,42 +11,42 @@ use rocket_contrib::{Json, Value};
 mod db;
 mod schema;
 
-mod customer;
-use customer::{Customer, NewCustomer};
+mod supplier;
+use supplier::{Supplier, NewSupplier};
 
-#[post("/", data = "<customer>", format = "application/json")]
-fn create(customer: Json<NewCustomer>, connection: db::Connection) -> Json<Customer> {
+#[post("/", data = "<supplier>", format = "application/json")]
+fn create(supplier: Json<NewSupplier>, connection: db::Connection) -> Json<Supplier> {
 
     // Parse the string of data into serde_json::Value.
-    let insert = NewCustomer { ..customer.into_inner() };
+    let insert = NewSupplier { ..supplier.into_inner() };
     println!("Please call {} at the number", insert.name);
-    Json(NewCustomer::create(insert, &connection))
+    Json(NewSupplier::create(insert, &connection))
 }
 
 #[get("/")]
 fn read(connection: db::Connection) -> Json<Value> {
-    Json(json!(Customer::read(&connection)))
+    Json(json!(Supplier::read(&connection)))
 }
 
-#[put("/<id>", data = "<customer>", format = "application/json")]
-fn update(id: i32, customer: Json<Customer>, connection: db::Connection) -> Json<Value> {
-    let update = Customer { id: id, ..customer.into_inner() };
+#[put("/<id>", data = "<supplier>", format = "application/json")]
+fn update(id: i32, supplier: Json<Supplier>, connection: db::Connection) -> Json<Value> {
+    let update = Supplier { id: id, ..supplier.into_inner() };
     Json(json!({
-        "success": Customer::update(id, update, &connection)
+        "success": Supplier::update(id, update, &connection)
     }))
 }
 
 #[delete("/<id>")]
 fn delete(id: i32, connection: db::Connection) -> Json<Value> {
     Json(json!({
-        "success": Customer::delete(id, &connection)
+        "success": Supplier::delete(id, &connection)
     }))
 }
 
 fn main() {
     rocket::ignite()
         .manage(db::connect())
-        .mount("/customer", routes![create, update, delete])
-        .mount("/customers", routes![read])
+        .mount("/supplier", routes![create, update, delete])
+        .mount("/suppliers", routes![read])
         .launch();
 }
--- src/schema.rs
+++ src/schema.rs
@@ -1,26 +1,18 @@
 table! {
-    category_of_trash (id) {
+    categories_of_trash (id) {
         id -> Int4,
         name -> Bpchar,
     }
 }
 
 table! {
-    customers (id) {
+    packages_of_partner (id) {
         id -> Int4,
-        name -> Bpchar,
-        email -> Bpchar,
-        password -> Bpchar,
-        phone_number -> Int4,
-    }
-}
-
-table! {
-    packet_of_partner (id) {
-        id -> Int4,
-        category_of_trash -> Int4,
+        category_of_trash_id -> Int4,
         min_weight -> Int4,
         price -> Int4,
+        partner_id -> Int4,
+        created_at -> Nullable<Timestamp>,
     }
 }
 
@@ -28,17 +20,40 @@ table! {
     partners (id) {
         id -> Int4,
         name -> Bpchar,
+        password -> Bpchar,
         phone_number -> Int4,
         area -> Bpchar,
-        packet -> Int4,
+        email -> Bpchar,
+        created_at -> Nullable<Timestamp>,
+    }
+}
+
+table! {
+    use diesel::types::String;
+    use super::User_role;
+    signin_log (id) {
+        id -> Int4,
+        user_id -> Int4,
+        user_group -> User_role,
+        created_at -> Nullable<Timestamp>,
+    }
+}
+
+table! {
+    suppliers (id) {
+        id -> Int4,
+        name -> Bpchar,
         email -> Bpchar,
         password -> Bpchar,
+        phone_number -> Int4,
+        created_at -> Nullable<Timestamp>,
     }
 }
 
 allow_tables_to_appear_in_same_query!(
-    category_of_trash,
-    customers,
-    packet_of_partner,
+    categories_of_trash,
+    packages_of_partner,
     partners,
+    signin_log,
+    suppliers,
 );
