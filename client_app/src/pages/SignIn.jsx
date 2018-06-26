b213a0cc823cddc76c4611359e1cad4d172dc81c
Tandry <syawaludintandry@yahoo.com>
2018-06-26 10:59:57 +0700

    UPDATED: schema for access css

--- client_app/src/pages/SignIn.jsx
+++ client_app/src/pages/SignIn.jsx
@@ -13,33 +13,34 @@ import {
 
 import NavbarWelcome from '../partials/NavbarWelcome';
 
-import style from '../styles/SignIn.scss';
-
+import '../style/SignIn.css';
 class SignIn extends Component {
   render() {
-    return <Fragment styleName="SignInPage">
-      <NavbarWelcome />
-      <Container className="singin_form_container">
-        <Row>
-          <Col md={{ size: 6, offset: 3 }}>
-            <Card>
-              <CardBody>
-                <Form>
-                  <FormGroup>
-                    <Label for="exampleEmail">Email</Label>
-                    <Input type="email" name="email" placeholder="please input valid email" />
-                  </FormGroup>
-                  <FormGroup>
-                    <Label for="exampleEmail">Password</Label>
-                    <Input type="password" name="email" placeholder="input your password" />
-                  </FormGroup>
-                </Form>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-    </Fragment>
+    return (
+      <Fragment>
+        <NavbarWelcome />
+        <Container className="singin_form_container">
+          <Row>
+            <Col md={{ size: 6, offset: 3 }}>
+              <Card>
+                <CardBody>
+                  <Form>
+                    <FormGroup>
+                      <Label for="exampleEmail">Email</Label>
+                      <Input type="email" name="email" placeholder="please input valid email" />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label for="exampleEmail">Password</Label>
+                      <Input type="password" name="email" placeholder="input your password" />
+                    </FormGroup>
+                  </Form>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </Fragment>
+    )
   }
 }
 
